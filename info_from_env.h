acd01605eb618893868404a83e9a606576f76c66
Wang Xinyong <wang.xy.chn@gmail.com>
2021-08-23 11:54:16 -0400

    proc_cpu: proc_mem: add use info from env support

---
+++ info_from_env.h
@@ -0,0 +1,11 @@
+#ifndef _INFO_FROM_ENV
+#define _INFO_FROM_ENV
+
+#include <unistd.h>
+#include <inttypes.h>
+
+int max_cpu_count_from_env(pid_t pid);
+
+uint64_t mem_limit_from_env(pid_t pid);
+
+#endif
